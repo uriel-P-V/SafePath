4436512c45105740f04a8b41b087a91e3b379963
Eduardo de Jesús Quintero Meza <eduardo.quintero5516@alumnos.udg.mx>
2025-09-10 02:24:54 -0600

    Mejora del navbar

--- js/navbar.js
+++ js/navbar.js
@@ -3,5 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
     .then(response => response.text())
     .then(data => {
       document.getElementById("navbar-container").innerHTML = data;
+      // Funcionalidad responsive estilo W3Schools
+      const navbar = document.getElementById("myNavbar");
+      const icon = navbar.querySelector('.icon');
+      if (icon && navbar) {
+        icon.addEventListener('click', function() {
+          if (navbar.className === "navbar") {
+            navbar.className += " responsive";
+          } else {
+            navbar.className = "navbar";
+          }
+        });
+      }
     });
 });
